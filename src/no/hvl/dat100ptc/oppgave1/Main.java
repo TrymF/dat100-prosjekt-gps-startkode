4fee1063a0478c93261cc50979a352da896a4b47
Trym Fanebust <669914@stud.hvl.no>
2024-10-04 23:16:06 +0200

    Update Main.java

--- src/no/hvl/dat100ptc/oppgave1/Main.java
+++ src/no/hvl/dat100ptc/oppgave1/Main.java
@@ -2,10 +2,15 @@ package no.hvl.dat100ptc.oppgave1;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-		// TODO
-		
-	}
+    public static void main(String[] args) {
+        GPSPoint gpsPoint = new GPSPoint(1, 2.0, 3.0, 5.0);
 
+        System.out.println("Tidspunkt: " + gpsPoint.getTime());
+
+        gpsPoint.setTime(2);
+
+   //     System.out.println("Nytt tidspunkt: " + gpsPoint.getTime());
+
+        System.out.println("GPSPoint informasjon: " + gpsPoint.toString());
+    }
 }
